f1e36ca851463d062b6058da9923c782da97a11d
Zach Cossman <Zachcos@gmail.com>
2020-09-29 19:16:12 -0400

    Wire up prev/next buttons to control testimonial text and images

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const nextBtn = function() {
+  document.querySelector(".image-slide").classList.add('l540')
+  document.querySelector(".testimonial-slide").classList.add('t280')
+}
+
+const prevBtn = function() {
+  document.querySelector(".image-slide").classList.remove('l540')
+  document.querySelector(".testimonial-slide").classList.remove('t280')
+}
